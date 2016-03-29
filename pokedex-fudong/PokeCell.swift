6402d0e5370058e651b1706f31a67751f40b8246
yangxiaofu <fudaviddong@gmail.com>
2016-03-28 20:52:53 -0600

    start

---
+++ pokedex-fudong/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  pokedex-fudong
+//
+//  Created by Fu Dong on 3/15/16.
+//  Copyright © 2016 Fu Dong. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg:UIImageView!
+    @IBOutlet weak var nameLbl:UILabel!
+    
+    var pokemon:Pokemon!
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon:Pokemon){
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
--- pokedex-fudong/ViewController.swift
+++ pokedex-fudong/ViewController.swift
@@ -8,19 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collection:UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collection.delegate = self
+        collection.dataSource = self
         
+        // Do any additional setup after loading the view, typically from a nib.
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell{
+            let pokemon = Pokemon(name: "Test", pokedexId: indexPath.row)
+            cell.configureCell(pokemon)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 718
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: 75,height: 75)
+    }
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
 
 
 }
